ee98c285a000b12feffcd2e514eb1af5ced1301a
RicardoClaro7 <167456148+RicardoClaro7@users.noreply.github.com>
2024-08-01 16:48:44 +0000

    first commit

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const SecondsCounter = ({ number }) => {
+    return <div className="digito">{number}</div>;
+};
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { SecondsCounter } from "./SecondsCounter.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [timer, setTimer] = useState(0);
+
+    useEffect(() => {       //useEffect: Le dice al reloj cuándo empezar y cuándo detenerse.
+        const intervalId = setInterval(() => {       //setInterval: Cada segundo, suma 1 al número.
+            setTimer(prevTimer => prevTimer + 1);     //setTimer(prevTimer => prevTimer + 1): Esto es lo que realmente suma 1 al número cada vez que pasa un segundo.
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return (
+        <main className="text-center">
+            <section className="counter-holder">
+                <SecondsCounter number={<span className="fa fa-clock"></span>} />
+                <SecondsCounter number={Math.floor(timer / 100000) % 10} />
+                <SecondsCounter number={Math.floor(timer / 10000) % 10} />
+                <SecondsCounter number={Math.floor(timer / 1000) % 10} />
+                <SecondsCounter number={Math.floor(timer / 100) % 10} />
+                <SecondsCounter number={Math.floor(timer / 10) % 10} />
+                <SecondsCounter number={timer % 10} />
+            </section>
+        </main>
+    );
 };
 
 export default Home;
